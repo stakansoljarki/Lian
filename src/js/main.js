9ad47292cd717644c61d559d4e83debf694528c0
stakansoljarki <roman.dybach@gmail.com>
2020-04-26 13:12:54 +0300

    removed unnecessary file and comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 $(function () {
-    // popup();
     slick();
-    // burger();
-    // search();
     $(document).ready(function () {
         $(".fa-search").click(function () {
             $(".wrap, .input").toggleClass("active");
